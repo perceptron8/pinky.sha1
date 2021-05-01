0fa17726bd7f21c823acdf285ad6c988a22bd4c3
perceptron8 <perceptron8@users.noreply.github.com>
2021-05-01 15:46:46 +0200

    generate-json.js + generate-rules.js

--- generate-json.js
+++ generate-json.js
@@ -1,13 +1,6 @@
 import fs from "fs";
 
-if (process.argv.length != 3) {
-	console.error(`usage: node ${process.argv[1]} filename`);
-	process.exit(-1);
-}
-
-const buffer = fs.readFileSync(`${process.argv[2]}`);
-const pattern = /^building_lib\/(buffad_)?c(ollectible)?_[a-z0-9_]*\.png$/;
-const mapping = [];
+const buffer = fs.readFileSync(0);
 
 let offset = 0;
 const readUTF = () => {
@@ -18,12 +11,11 @@ const readUTF = () => {
 	return string;
 };
 
+const mapping = [];
 while (offset < buffer.length) {
 	const file = readUTF();
 	const hash = readUTF();
-	if (file.match(pattern)) {
-		mapping.push([file, hash]);
-	}
+	mapping.push([file, hash]);
 }
 
-console.log(mapping);
+console.log(JSON.stringify(mapping, null, 2));
---
+++ generate-rules.js
@@ -0,0 +1,21 @@
+import fs from "fs";
+
+const text = fs.readFileSync(0, "utf8");
+const mapping = JSON.parse(text);
+
+const base = "https://ubistatic-a.akamaihd.net/0018/base/GFX_HASHED";
+const pattern = /^building_lib\/(buffad_)?c(ollectible)?_[a-z0-9_]*\.png$/;
+const replacement = "f9f7e2bacd84c76001820a3621bda5c6959d609d.png";
+
+const rules = [];
+for (const [file, hash] of mapping) {
+	if (file.match(pattern)) {
+		rules.push({
+			"id": rules.length + 1,
+			"condition": { "urlFilter": `${base}/building_lib/${hash}`, "resourceTypes": ["xmlhttprequest"] },
+			"action": { "type": "redirect", "redirect": { "url": `${base}/building_lib/${replacement}` } }
+		});
+	}
+}
+
+console.log(JSON.stringify(rules, null, 2));
